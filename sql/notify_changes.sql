fee7bf17c54cd074a3b78c707639f8a5e8228d4f
peepee <ppark9553@gmail.com>
2020-05-07 22:10:08 +0900

    changes in sql syntax error

--- sql/notify_changes.sql
+++ sql/notify_changes.sql
@@ -4,8 +4,8 @@ BEGIN
     PERFORM pg_notify(
         'controlstate_changed',
         json_build_object(
-            'operation': TG_OP,
-            'record': row_to_json(NEW)
+            'operation', TG_OP,
+            'record', row_to_json(NEW)
         )::text
     );
 
